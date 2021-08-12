bad3f636db1e82b93a7bc66a64d423ddacba5594
Miguel Palhas <mpalhas@gmail.com>
2021-08-12 17:10:37 +0100

    code review

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 // import "@typechain/hardhat";
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-web3";
 
 import { HardhatUserConfig } from "hardhat/config";
 

f7a20819dffff1b67cf7596d4e3af9c79e8376e0
Miguel Palhas <mpalhas@gmail.com>
2021-08-31 08:48:16 +0100

    Upgrade compiler to 0.8.7 (#10)

--- contracts/token/TestERC20.sol
+++ contracts/token/TestERC20.sol
@@ -1,17 +1,21 @@
-pragma solidity 0.4.25;
+pragma solidity 0.8.7;
 
-import "./ERC20Detailed.sol";
-import "./ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract TestERC20 is ERC20Detailed, ERC20 {
+contract TestERC20 is ERC20 {
+    uint8 private customDecimals;
 
     constructor(
-        string _name,
-        string _symbol,
+        string memory _name,
+        string memory _symbol,
         uint8 _decimals,
         uint _initialSupply
-    ) public ERC20Detailed(_name, _symbol, _decimals) {
+    ) ERC20(_name, _symbol) {
+        customDecimals = _decimals;
         _mint(msg.sender, _initialSupply);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return customDecimals;
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,7 +14,7 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.4.25",
+    version: "0.8.7",
     settings: {
       optimizer: {
         enabled: true,

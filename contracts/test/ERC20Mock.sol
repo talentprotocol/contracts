85d395627edfcfb340c1cfa2cb1aae12339af3f5
Miguel Palhas <mpalhas@gmail.com>
2021-09-13 20:18:06 +0100

    code review

--- contracts/test/ERC20Mock.sol
+++ contracts/test/ERC20Mock.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.3;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract ERC20Mock is ERC20 {
   constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    console.log(1000 ether);
+    console.log(1000 * 10**18);
     _mint(msg.sender, 1000 * 10**18);
   }
 }

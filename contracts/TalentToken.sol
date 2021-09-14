f464cca0a85d4a4119c845128e44f35440546003
Miguel Palhas <mpalhas@gmail.com>
2021-09-14 17:33:25 +0100

    code review

--- contracts/TalentToken.sol
+++ contracts/TalentToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.7;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";

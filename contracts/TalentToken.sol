506b7dd2565b7f4f649f51e0b37d2ba007918e9f
Miguel Palhas <mpalhas@gmail.com>
2021-09-14 10:55:25 +0100

    Ability to mint & burn talent tokens (#22)

--- contracts/TalentToken.sol
+++ contracts/TalentToken.sol
@@ -4,21 +4,49 @@ pragma solidity ^0.8.7;
 
 import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
 /// @title The base contract for Talent Tokens
-contract TalentToken is ERC20, ERC165 {
+contract TalentToken is ERC20, ERC165, AccessControl {
+  /// minter role
+  bytes32 public constant ROLE_MINTER_BURNER = keccak256("MINTER_BURNER");
+
   constructor(
     string memory _name,
     string memory _symbol,
     uint _initialSupply,
-    address _talent
+    address _talent,
+    address _minter_burner
   ) ERC20(_name, _symbol) {
     _mint(_talent, _initialSupply);
+
+    _setupRole(ROLE_MINTER_BURNER, _minter_burner);
   }
 
   /// @inheritdoc ERC165
-  function supportsInterface(bytes4 interfaceId) public pure override(ERC165) returns (bool) {
+  function supportsInterface(bytes4 interfaceId) public view override(ERC165, AccessControl) returns (bool) {
     return interfaceId == type(ERC165).interfaceId
-      || interfaceId == type(IERC20).interfaceId;
+      || interfaceId == type(IERC20).interfaceId
+      || AccessControl.supportsInterface(interfaceId);
+  }
+
+  /// Mints new supply
+  ///
+  /// @notice Only accessible to the role MINTER_BURNER
+  ///
+  /// @param _to Recipient of the new tokens
+  /// @param _amount Amount to mint
+  function mint(address _to, uint _amount) public onlyRole(ROLE_MINTER_BURNER) {
+    _mint(_to, _amount);
+  }
+
+  /// Burns existing supply
+  ///
+  /// @notice Only accessible to the role MINTER_BURNER
+  ///
+  /// @param _from Owner of the tokens to burn
+  /// @param _amount Amount to mint
+  function burn(address _from, uint _amount) public onlyRole(ROLE_MINTER_BURNER) {
+    _burn(_from, _amount);
   }
 }

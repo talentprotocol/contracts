a8338cd9fb50333fb6936f7c179a330999aa5718
Miguel Palhas <mpalhas@gmail.com>
2021-09-24 19:46:04 +0100

    Staking events

--- test/shared/utils.ts
+++ test/shared/utils.ts
@@ -10,7 +10,9 @@ import { TalentToken__factory } from "../../typechain";
 export async function findEvent(tx: ContractTransaction, name: string): Promise<Event | undefined> {
   const receipt: ContractReceipt = await tx.wait();
 
-  return receipt.events?.find((e) => e.event === name);
+  return receipt.events?.find((e) => {
+    return e.event === name;
+  });
 }
 
 export async function deployTalentToken(

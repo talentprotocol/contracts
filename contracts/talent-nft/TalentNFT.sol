991e5f30fed83553206d72c8dbbecb06c809c9e2
helloiambguedes <bguedesdev@gmail.com>
2022-10-21 23:55:31 +0100

    wip: contract refactor

--- contracts/talent-nft/TalentNFT.sol
+++ contracts/talent-nft/TalentNFT.sol
@@ -6,11 +6,6 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import "./model/Tiers.sol";
 
-struct WhiteListEntry {
-    TIERS tier;
-    bool isDefined;
-}
-
 contract TalentNFT is ERC721, ERC721Enumerable, AccessControl {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -18,36 +13,64 @@ contract TalentNFT is ERC721, ERC721Enumerable, AccessControl {
     string private _baseURIExtended;
     mapping (uint256 => string) _tokenURIs;
     bool private _publicStageFlag = false;
-    mapping(address => WhiteListEntry) _whitelist;
+    mapping(address => TIERS) _whitelist;
 
     constructor(address _owner, string memory _ticker) ERC721("Talent Protocol NFT Collection", _ticker) {
       _setupRole(DEFAULT_ADMIN_ROLE, _owner);
     }
 
-    function whiteListAddress(address _to, TIERS tier) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _whitelist[_to].tier = tier;
-        _whitelist[_to].isDefined = true;
+    /**
+        Public stage status setter
+        set's _publicStageFlag
+     */
+    function setPublicStageFlag(bool newFlagValue) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(newFlagValue != _publicStageFlag, 
+            "Unable to change _publicStageFlag value because the new value is the same as the current one");
+        _publicStageFlag = newFlagValue;
     }
 
-    function isWhiteListed(address account) private returns (bool) {
+    /**
+        Public stage status getter
+        returns _publicStageFlag from contract state
+     */
+    function getPublicStageFlag() public view returns (bool) {
+        return _publicStageFlag;
+    }
+
+    /**
+        isWhitelisted should only be called if the getPublicStageFlag is false
+        This means the public can't freely mint Talent NFT's
+
+        returns associated TIER with the account if the account is whitelisted
+            OR TIERS.PUBLIC_STAGE if public stage is active
+            OR TIERS.UNDEFINED if the account is not whitelisted
+     */
+    function checkAccountTier(address account) private pure returns (TIERS) {
         if (_publicStageFlag) {
-            return true;
+            return TIERS.PUBLIC_STAGE;
         }
-        return _whitelist[account].isDefined;
+        if (_whitelist[account] != TIERS.UNDEFINED) {
+            return TIERS.UNDEFINED;
+        }
+        return _whitelist[account];
+    }
+
+    function isWhitelisted(address account) public returns (bool) {
+        return checkAccountTier(account) == TIERS.UNDEFINED;
+    }
+
+    function whitelistAddress(address _to, TIERS tier) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _whitelist[_to] = tier;
     }
 
     function mint(address _to) public {
-        if (!isWhiteListed(_to)) {
+        if (!isWhitelisted(_to)) {
             require(false, "Minting not allowed for account roles");
         }
 
         _tokenIds.increment();
         uint256 id = _tokenIds.current();
         _safeMint(msg.sender, id);
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return _baseURIExtended;
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
@@ -57,20 +80,21 @@ contract TalentNFT is ERC721, ERC721Enumerable, AccessControl {
         return base;
     }
 
+    function setTokenURI(uint256 tokenId, string memory tokenURI_) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        _tokenURIs[tokenId] = tokenURI_;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIExtended;
+    }
+
     function setBaseURI(string memory baseURI_) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _baseURIExtended = baseURI_;
     }
 
     function addOwner(address _newOwner) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _setupRole(DEFAULT_ADMIN_ROLE, _newOwner);
-    }
-
-    function getPublicStageFlag() public view returns (bool) {
-        return _publicStageFlag;
-    }
-
-    function setPublicStageFlag(bool newFlagValue) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        _publicStageFlag = newFlagValue;
     }
 
     // Disable transfering this NFT

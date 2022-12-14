8dc88b4d9cd1885ca78b6867b33ba715a59f9b76
francisco-leal <francisco@talentprotocol.com>
2022-12-14 15:18:12 +0000

    Add metadata to deploy task

--- scripts/deployTalentNFT.ts
+++ scripts/deployTalentNFT.ts
@@ -8,6 +8,13 @@ async function main() {
   console.log(`Owner will be ${creator.address}`);
   const nftContract = await deployTalentNFT(creator.address, "TalentNFT");
   console.log(`TalentNFT Address: ${nftContract.address}`);
+
+  // @TODO: change to a better URI
+  const URI = "ipfs://bafyreifuc7inyu6fhytj2vof6qhrejkla7ohd7qwac33gfwcc57mrbxfn4/metadata.json";
+  await nftContract.setBaseURI(URI);
+
+  console.log("Change the URI to: ", URI);
+
   //console.log("Assigning one NFT to the creator");
   //console.log(await nftContract.connect(creator).mint("1.png"));
   console.log("Done");

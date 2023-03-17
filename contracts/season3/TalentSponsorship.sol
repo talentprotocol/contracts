00c6bd79b40ee9f3115bc60865d7dd56a0336a3b
francisco-leal <francisco@talentprotocol.com>
2023-03-17 01:31:49 +0000

    Add sponsorship base contract

---
+++ contracts/season3/TalentSponsorship.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract TalentSponsorship {
+  // sponsor => talent => token => amount
+  mapping(address => mapping(address => mapping(address => uint256))) public sponsorships;
+
+  // amount available for talent
+  mapping(address => mapping(address => uint256)) public amountAvailableForTalent;
+
+  // totalNumberOfSponsorships
+  uint256 public totalSponsorships;
+
+  // A new sponsorship has been created
+  event SponsorshipCreated(
+    address indexed sponsor,
+    address indexed talent,
+    uint256 amount,
+    address indexed token,
+    string symbol
+  );
+
+  // Talent Withdrew a given Token
+  event Withdraw(address indexed talent, uint256 amount, address indexed token, string symbol);
+
+  constructor() {
+    totalSponsorships = 0;
+  }
+
+  // Create a new sponsor position
+  function sponsor(address _talent, uint256 _amount, address _token) public
+  {
+    require(IERC20(_token).balanceOf(msg.sender) >= _amount, "You don't have enough balance");
+    require(IERC20(_token).allowance(
+      msg.sender, address(this)) >= _amount,
+      "You must first approve this smart contract to transfer"
+    );
+
+    // setup the internal state
+    totalSponsorships = totalSponsorships + 1;
+    sponsorships[msg.sender][_talent][_token] = sponsorships[msg.sender][_talent][_token] + _amount;
+    amountAvailableForTalent[_talent][_token] = amountAvailableForTalent[_talent][_token] + _amount;
+
+    // transfer from sender => contract
+    IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+
+    // emit event
+    string memory symbol = IERC20Metadata(_token).symbol();
+    emit SponsorshipCreated(msg.sender, _talent, _amount, _token, symbol);
+  }
+
+  // Withdraw all funds for a given token
+  function withdrawToken(address _token) public
+  {
+    require(amountAvailableForTalent[msg.sender][_token] >= 0, "There are no funds for you to retrieve");
+
+    // setup the internal state
+    uint256 amount = amountAvailableForTalent[msg.sender][_token];
+
+    require(IERC20(_token).balanceOf(address(this)) >= amount, "Smart contract does not have enough funds");
+    amountAvailableForTalent[msg.sender][_token] = amountAvailableForTalent[msg.sender][_token] - amount;
+
+    // transfer from contract => sender
+    IERC20(_token).transfer(msg.sender, amount);
+
+    // emit event
+    string memory symbol = IERC20Metadata(_token).symbol();
+    emit Withdraw(msg.sender, amount, _token, symbol);
+  }
+
+  function amountAvailable(address _token) public view returns (uint256)
+  {
+    return amountAvailableForTalent[msg.sender][_token];
+  }
+}
---
+++ test/contracts/season3/TalentSponsorship.ts
@@ -0,0 +1,87 @@
+import chai from "chai";
+import { ethers, waffle } from "hardhat";
+import { solidity } from "ethereum-waffle";
+
+import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+import { USDTMock, TalentSponsorship } from "../../../typechain-types";
+import { Artifacts } from "../../shared";
+
+import { findEvent } from "../../shared/utils";
+
+chai.use(solidity);
+
+const { expect } = chai;
+const { parseUnits } = ethers.utils;
+const { deployContract } = waffle;
+
+describe("VirtualTAL", () => {
+  let admin: SignerWithAddress;
+  let supporter: SignerWithAddress;
+  let talent: SignerWithAddress;
+
+  let contract: TalentSponsorship;
+  let stable: USDTMock;
+
+  beforeEach(async () => {
+    [admin, supporter, talent] = await ethers.getSigners();
+
+    stable = (await deployContract(admin, Artifacts.USDTMock, [])) as USDTMock;
+
+    await stable.connect(admin).transfer(supporter.address, parseUnits("10000"));
+  });
+
+  async function builder() {
+    return deployContract(admin, Artifacts.TalentSponsorship, []);
+  }
+
+  describe("behaviour", () => {
+    beforeEach(async () => {
+      contract = await builder() as TalentSponsorship;
+    });
+
+    it("is created with no state", async () => {
+      expect(await contract.totalSponsorships()).to.eq(0);
+    });
+
+    it("emits a SponsorshipCreated event everytime a sponsorship is created", async () => {
+      const amount = parseUnits("10");
+      await stable.connect(supporter).approve(contract.address, amount);
+
+      const tx = await contract.connect(supporter).sponsor(talent.address, amount, stable.address);
+
+      const event = await findEvent(tx, "SponsorshipCreated");
+
+      expect(event).to.exist;
+      expect(event?.args?.sponsor).to.eq(supporter.address);
+      expect(event?.args?.talent).to.be.eq(talent.address);
+      expect(event?.args?.amount).to.equal(amount);
+      expect(event?.args?.token).to.equal(stable.address);
+      expect(event?.args?.symbol).to.equal(await stable.symbol());
+
+      expect(await stable.balanceOf(contract.address)).to.eq(amount);
+    });
+
+    it("emits a Withdraw Event everytime a talent withdraws", async () => {
+      const amount = parseUnits("10");
+      await stable.connect(supporter).approve(contract.address, amount);
+
+      await contract.connect(supporter).sponsor(talent.address, amount, stable.address);
+
+      expect(await contract.connect(talent).amountAvailable(stable.address)).to.eq(amount)
+      expect(await stable.balanceOf(contract.address)).to.eq(amount);
+
+      const tx = await contract.connect(talent).withdrawToken(stable.address);
+
+      const event = await findEvent(tx, "Withdraw");
+
+      expect(event).to.exist;
+      expect(event?.args?.talent).to.be.eq(talent.address);
+      expect(event?.args?.amount).to.equal(amount);
+      expect(event?.args?.token).to.equal(stable.address);
+      expect(event?.args?.symbol).to.equal(await stable.symbol());
+
+      expect(await stable.balanceOf(talent.address)).to.eq(amount);
+    });
+  });
+});

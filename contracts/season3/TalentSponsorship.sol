28ddd2a56c4112d98acc80b5de2273de5708f823
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2023-04-18 16:54:38 +0100

    Optimize smart contract

--- contracts/season3/TalentSponsorship.sol
+++ contracts/season3/TalentSponsorship.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract TalentSponsorship is Ownable {
+    using SafeERC20 for IERC20Metadata;
+
     // sponsor => talent => token => totalAmount
     mapping(address => mapping(address => mapping(address => uint256))) public sponsorships;
 
@@ -55,9 +58,6 @@ contract TalentSponsorship is Ownable {
     );
 
     constructor(address contractOwner) {
-        totalSponsorships = 0;
-        totalRevokedSponsorships = 0;
-        totalClaimedSponsorships = 0;
         transferOwnership(contractOwner);
     }
 
@@ -68,22 +68,22 @@ contract TalentSponsorship is Ownable {
 
     // Create a new sponsor position
     function sponsor(address _talent, uint256 _amount, address _token) public onlyWhileEnabled {
-        require(IERC20(_token).balanceOf(msg.sender) >= _amount, "You don't have enough balance");
-        require(
-            IERC20(_token).allowance(msg.sender, address(this)) >= _amount,
-            "You must first approve this smart contract to transfer"
-        );
-
-        // setup the internal state
-        totalSponsorships = totalSponsorships + 1;
-        amountAvailableForTalent[_talent][_token] = amountAvailableForTalent[_talent][_token] + _amount;
-        sponsorships[msg.sender][_talent][_token] = sponsorships[msg.sender][_talent][_token] + _amount;
-        amountAvailableForSponsorship[msg.sender][_talent][_token] =
-            amountAvailableForSponsorship[msg.sender][_talent][_token] +
-            _amount;
+        require(_talent != address(0), "Invalid talent");
+        require(_amount > 0, "Invalid amount");
+        require(IERC20Metadata(_token).balanceOf(msg.sender) >= _amount, "You don't have enough balance");
+        require(IERC20Metadata(_token).allowance(msg.sender, address(this)) >= _amount, "Must approve contract first");
 
         // transfer from sender => contract
-        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        IERC20Metadata(_token).safeTransferFrom(msg.sender, address(this), _amount);
+
+        // setup the internal state
+        totalSponsorships = SafeMath.add(totalSponsorships, 1);
+        amountAvailableForTalent[_talent][_token] = SafeMath.add(amountAvailableForTalent[_talent][_token], _amount);
+        sponsorships[msg.sender][_talent][_token] = SafeMath.add(sponsorships[msg.sender][_talent][_token], _amount);
+        amountAvailableForSponsorship[msg.sender][_talent][_token] = SafeMath.add(
+            amountAvailableForSponsorship[msg.sender][_talent][_token],
+            _amount
+        );
 
         // emit event
         string memory symbol = IERC20Metadata(_token).symbol();
@@ -92,6 +92,8 @@ contract TalentSponsorship is Ownable {
 
     // Withdraw all funds for a given token
     function withdrawToken(address _sponsor, address _token) public {
+        require(_sponsor != address(0), "Invalid sponsor");
+        require(_token != address(0), "Invalid token");
         require(
             amountAvailableForSponsorship[_sponsor][msg.sender][_token] > 0,
             "There are no funds for you to retrieve"
@@ -99,16 +101,20 @@ contract TalentSponsorship is Ownable {
 
         // setup the internal state
         uint256 amount = amountAvailableForSponsorship[_sponsor][msg.sender][_token];
-        require(IERC20(_token).balanceOf(address(this)) >= amount, "Smart contract does not have enough funds");
+        require(IERC20Metadata(_token).balanceOf(address(this)) >= amount, "Not enough funds on contract");
 
-        totalClaimedSponsorships = totalClaimedSponsorships + 1;
-        amountAvailableForSponsorship[_sponsor][msg.sender][_token] =
-            amountAvailableForSponsorship[_sponsor][msg.sender][_token] -
-            amount;
-        amountAvailableForTalent[msg.sender][_token] = amountAvailableForTalent[msg.sender][_token] - amount;
+        totalClaimedSponsorships = SafeMath.add(totalClaimedSponsorships, 1);
+        amountAvailableForSponsorship[_sponsor][msg.sender][_token] = SafeMath.sub(
+            amountAvailableForSponsorship[_sponsor][msg.sender][_token],
+            amount
+        );
+        amountAvailableForTalent[msg.sender][_token] = SafeMath.sub(
+            amountAvailableForTalent[msg.sender][_token],
+            amount
+        );
 
         // transfer from contract => sender
-        IERC20(_token).transfer(msg.sender, amount);
+        IERC20Metadata(_token).safeTransfer(msg.sender, amount);
 
         // emit event
         string memory symbol = IERC20Metadata(_token).symbol();
@@ -117,20 +123,23 @@ contract TalentSponsorship is Ownable {
 
     // Revokes a sponsor position
     function revokeSponsor(address _talent, address _token) public {
+        require(_talent != address(0), "Invalid talent");
+        require(_token != address(0), "Invalid token");
         uint256 amount = amountAvailableForSponsorship[msg.sender][_talent][_token];
 
         require(amount > 0, "There's no pending sponsorship to revoke");
-        require(IERC20(_token).balanceOf(address(this)) >= amount, "The contract don't have enough balance");
+        require(IERC20Metadata(_token).balanceOf(address(this)) >= amount, "Not enough balance on contract");
 
         // setup the internal state
-        totalRevokedSponsorships = totalRevokedSponsorships + 1;
-        amountAvailableForTalent[_talent][_token] = amountAvailableForTalent[_talent][_token] - amount;
-        amountAvailableForSponsorship[msg.sender][_talent][_token] =
-            amountAvailableForSponsorship[msg.sender][_talent][_token] -
-            amount;
+        totalRevokedSponsorships = SafeMath.add(totalRevokedSponsorships, 1);
+        amountAvailableForTalent[_talent][_token] = SafeMath.sub(amountAvailableForTalent[_talent][_token], amount);
+        amountAvailableForSponsorship[msg.sender][_talent][_token] = SafeMath.sub(
+            amountAvailableForSponsorship[msg.sender][_talent][_token],
+            amount
+        );
 
         // transfer from contract => sender
-        IERC20(_token).transfer(msg.sender, amount);
+        IERC20Metadata(_token).safeTransfer(msg.sender, amount);
 
         // emit event
         string memory symbol = IERC20Metadata(_token).symbol();

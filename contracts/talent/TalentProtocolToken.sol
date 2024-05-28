63afa1df0265e5578df74bb6e02e0c63737ad198
francisco-leal <francisco@talentprotocol.com>
2024-05-28 10:14:39 +0100

    Remove ERC20Permit and add more test cases

--- contracts/talent/TalentProtocolToken.sol
+++ contracts/talent/TalentProtocolToken.sol
@@ -3,24 +3,21 @@ pragma solidity ^0.8.24;
 
 // Import OpenZeppelin contracts
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract TalentProtocolToken is ERC20, ERC20Burnable, Pausable, ERC20Permit, Ownable {
+contract TalentProtocolToken is ERC20, ERC20Burnable, Pausable, Ownable {
   // Mint 1B tokens to the initial owner and pause the contract
   constructor(address initialOwner)
     ERC20("TalentProtocolToken", "TALENT")
-    ERC20Permit("TalentProtocolToken")
     Ownable(initialOwner)
   {
     _mint(initialOwner, 1_000_000_000 ether);
     _pause();
   }
 
-  function _update(address from, address to, uint256 value) internal virtual override(ERC20) {
+  function _update(address from, address to, uint256 value) internal override(ERC20) {
     require(to != address(this), "TalentProtocolToken: cannot transfer tokens to self");
     require(!paused() || owner() == _msgSender(), "Token transfer is not enabled while paused");
     super._update(from, to, value);

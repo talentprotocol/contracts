90c16f802a39310a901fdb7eb231f83e1ae0d59a
Francisco Leal <francisco@talentprotocol.com>
2024-08-06 17:55:55 +0100

    testnet deploy scripts update

--- scripts/passport/deployScorer.ts
+++ scripts/passport/deployScorer.ts
@@ -1,6 +1,6 @@
 import { ethers, network } from "hardhat";
 
-import { deployPassportBuilderScore } from "../shared";
+import { deployPassportBuilderScore, deployPassportSources, deploySmartScorer } from "../shared";
 
 async function main() {
   console.log(`Deploying passport builder score at ${network.name}`);
@@ -9,11 +9,26 @@ async function main() {
 
   console.log(`Admin will be ${admin.address}`);
 
-  // @TODO: Replace with registry address
-  const passport = await deployPassportBuilderScore("0x0", admin.address);
+  // base sepolia: 0xa600b3356c1440b6d6e57b0b7862dc3dfb66bc43
+  // base mainnet: 0xb477A9BD2547ad61f4Ac22113172Dd909E5B2331
+  const passportRegistry = "0xa600b3356c1440b6d6e57b0b7862dc3dfb66bc43";
 
-  console.log(`Scorer Address: ${passport.address}`);
-  console.log(`Scorer owner: ${await passport.owner()}`);
+  const scorer = await deployPassportBuilderScore(passportRegistry, admin.address);
+  console.log(`Scorer Address: ${scorer.address}`);
+  console.log(`Scorer owner: ${await scorer.owner()}`);
+
+  const sources = await deployPassportSources(admin.address);
+  console.log(`Sources Address: ${sources.address}`);
+  console.log(`Sources owner: ${await sources.owner()}`);
+
+  const smartScorer = await deploySmartScorer(
+    admin.address,
+    scorer.address,
+    sources.address,
+    passportRegistry,
+    admin.address
+  );
+  console.log(`Smart Scorer Address: ${smartScorer.address}`);
 
   console.log("Done");
 }

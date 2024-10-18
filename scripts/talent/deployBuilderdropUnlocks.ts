8418b4ae542732c756fca9f7ef8c45b40176c90e
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2024-10-18 12:28:01 +0100

    Add generation scripts for TGE unlocks

--- scripts/talent/deployBuilderdropUnlocks.ts
+++ scripts/talent/deployBuilderdropUnlocks.ts
@@ -10,7 +10,7 @@ import distributionSetup from "../data/summerBuilderdrop.json";
 import { createClient } from "@supabase/supabase-js";
 
 const TALENT_TOKEN_ADDRESS_TESTNET =  "0xb669707B3784B1284f6B6a398f6b04b1AD78C74E";
-const TALENT_TOKEN_ADDRESS_MAINNET =  "0xb669707B3784B1284f6B6a398f6b04b1AD78C74E";
+const TALENT_TOKEN_ADDRESS_MAINNET =  "0x9a33406165f562E16C3abD82fd1185482E01b49a";
 
 const VESTING_CATEGORY = "summer_builderdrop"
 
@@ -31,17 +31,9 @@ async function main() {
 
   console.log(`Admin will be ${admin.address}`);
 
-  if(!process.env.PUBLIC_SUPABASE_URL) {
-    console.error("Missing PUBLIC_SUPABASE_URL");
-    return 0;
-  }
-
-  if(!process.env.PUBLIC_SUPABASE_ANON_KEY) {
-    console.error("Missing PUBLIC_SUPABASE_ANON_KEY");
-    return 0;
-  }
-
   const allResults = distributionSetup as { amount: string; wallet: string }[];
+
+  console.log("Generate merkle tree");
 
   const merkleBase = allResults.reduce((acc, { wallet, amount }) => {
     acc[wallet.toLowerCase()] = ethers.utils.parseEther(amount).toBigInt();
@@ -49,47 +41,42 @@ async function main() {
   }, {} as Record<string, bigint>);
 
   const merkleTree = generateMerkleTree(merkleBase);
-  console.log("Generated merkle trees: ", merkleTree.root);
-  const tgeUnlockDistribution = await deployTalentTGEUnlock(TALENT_TOKEN_ADDRESS_TESTNET, admin.address, merkleTree.root);
 
-  console.log(`TGE Unlock distribution deployed at ${tgeUnlockDistribution.address}`);
+  console.log("Generate proof list");
+
+  let index = 0;
+  let fileIndex = 0;
+  fs.writeFileSync(`scripts/data/builderdrop-proofs-${fileIndex}.json`, "[")
+
   const proofList = allResults.map(({ wallet, amount }) => {
-    const value = ethers.utils.parseEther(amount);
+    const value = ethers.utils.parseEther(amount).toBigInt();
     const proof = merkleTree.getProof([wallet.toLowerCase(), value]);
+
+    index += 1;
+
+    if(index % 100000 == 0) {
+      fs.appendFileSync(`scripts/data/builderdrop-proofs-${fileIndex}.json`, "]")
+      fileIndex +=1
+      fs.writeFileSync(`scripts/data/builderdrop-proofs-${fileIndex}.json`, "[")
+    }
+
+    const message = JSON.stringify({wallet, proof})
+
+    fs.appendFileSync(`scripts/data/builderdrop-proofs-${fileIndex}.json`, `${message},\n`)
+    
     return {
       wallet,
-      value,
       proof,
     };
   });
 
-  console.log("Writing proofs to file");
-  fs.writeFileSync(
-    `./data/${VESTING_CATEGORY}-proofs.json`,
-    JSON.stringify(proofList, (key, value) => (typeof value === "bigint" ? value.toString() : value))
-  );
+  fs.appendFileSync(`scripts/data/builderdrop-proofs-${fileIndex}.json`, "]")
 
-  console.log("Uploading proofs to database");
+  console.log(`Contract init args: ${TALENT_TOKEN_ADDRESS_TESTNET} ${merkleTree.root} ${admin.address}`)
 
-  const supabase = createClient(process.env.PUBLIC_SUPABASE_URL, process.env.PUBLIC_SUPABASE_ANON_KEY)
+  const tgeUnlockDistribution = await deployTalentTGEUnlock(TALENT_TOKEN_ADDRESS_TESTNET, admin.address, merkleTree.root);
 
-  const proofsCount = proofList.length
-  for (let i = 0; i < proofsCount; i++) {
-    const element = proofList[i]
-
-    console.log(`Uploading ${i + 1}/${proofsCount}: ${element.wallet}`)
-
-    const { error } = await supabase
-      .from("distributions")
-      .update({ proof: element.proof })
-      .eq("wallet", element.wallet)
-      .eq("vesting_category", VESTING_CATEGORY)
-
-    if(error) {
-      console.error(error);
-    }
-  }
-
+  console.log(`TGE Unlock distribution deployed at ${tgeUnlockDistribution.address}`);
 
   console.log("Done");
 }

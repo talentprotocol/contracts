2bcb8de75dc95e4b06f0845f45bad9d5b7924d8d
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-10-31 09:05:42 +0200

    Removed the import of hardhat console

--- contracts/talent/TalentVault.sol
+++ contracts/talent/TalentVault.sol
@@ -8,8 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "../passport/PassportBuilderScore.sol";
 
-import "hardhat/console.sol";
-
 error ContractInsolvent();
 error InsufficientAllowance();
 error InsufficientBalance();

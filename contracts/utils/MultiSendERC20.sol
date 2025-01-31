8d9a1dbdca9af59fc7e5073204c6fb33c12f2d23
Francisco Leal <francisco@talentprotocol.com>
2025-01-31 23:20:27 +0000

    create simple contract for multi send

---
+++ contracts/utils/MultiSendERC20.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MultiSendERC20 is Ownable {
+  uint8 public arrayLimit;
+  ERC20 public token;
+
+  event Multisended(uint256 total, address token);
+
+  constructor(address _owner, address _token) Ownable(_owner) {
+    arrayLimit = 200;
+    token = ERC20(_token);
+  }
+
+  function setArrayLimit(uint8 _arrayLimit) external onlyOwner {
+    arrayLimit = _arrayLimit;
+  }
+
+  function multisendToken(address[] calldata _recipients, uint256[] calldata _amounts) public {
+    uint256 total = 0;
+    require(_recipients.length <= arrayLimit, "Array length exceeds limit");
+    uint8 i = 0;
+    for (i; i < _recipients.length; i++) {
+      token.transferFrom(msg.sender, _recipients[i], _amounts[i]);
+      total += _amounts[i];
+    }
+
+    emit Multisended(total, address(token));
+  }
+}
